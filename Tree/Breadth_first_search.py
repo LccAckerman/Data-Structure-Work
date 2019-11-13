eb93529d599e7fa74b80ea46865c5f9561805c54
LccAckerman <51859515+LccAckerman@users.noreply.github.com>
2019-11-13 20:46:49 +0800

    广度优先搜索

---
+++ Tree/Breadth_first_search.py
@@ -0,0 +1,79 @@
+class Node(object):
+    def __init__(self, value= None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_right_child(self):
+        return self.right
+
+    def get_left_child(self):
+        return self.left
+
+    def has_left_child(self):
+        return self.left != None
+
+    def has_right_child(self):
+        return self.right != None
+
+    def __repr__(self):
+        return f"Node{self.get_value()}"
+
+    def __str__(self):
+        return f"Node{self.get_value()}"
+
+class Tree:
+    def __init__(self, value = None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+# create a tree and add some nodes
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+
+#  Define a queue class
+from collections import deque
+class Queue():
+    def __init__(self):
+        self.q = deque()
+    def enq(self,value):
+        self.q.appendleft(value)
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+    def __len__(self):
+        return len(self.p)
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
+
+q = Queue()
+q.enq("apple")
+q.enq("banana")
+q.enq("cherry")
+print(q)
